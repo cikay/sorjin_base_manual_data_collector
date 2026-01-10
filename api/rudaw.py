00dee9e63be6c397c52e4ca101b4363d14a9a584
Muzaffer Cıkay <muzaffercky@gmail.com>
2026-01-10 20:05:12 +0300

    Fix rudaw api

--- api/rudaw.py
+++ api/rudaw.py
@@ -12,26 +12,26 @@ CSV_FILENAME = ".collected_data/news/rudaw.csv"
 def fetch_articles():
     file_exists = os.path.exists(CSV_FILENAME)
     with open(CSV_FILENAME, "a", newline="", encoding="utf-8") as csvfile:
-        fieldnames = ["title", "url", "content"]
+        fieldnames = ["title", "url", "text"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        if not file_exists:
-            start_page = 1
-            writer.writeheader()
-        else:
-            start_page = 1148
+        writer.writeheader()
 
-        fetch_article_content(writer, start_page)
+        fetch_article_content(writer)
 
 
-def fetch_article_content(writer, start_page=1):
-    max_page = 1590
-    for page in range(start_page, max_page + 1):
-        response = requests.get(URL.format(page))
+def fetch_article_content(writer):
+    max_page = 1000
+    current_page = 1
+    while True:
+        response = requests.get(URL.format(current_page))
         if response.status_code != 200:
-            print(f"Failed to retrieve page {page}: {response.status_code}")
-            raise Exception(f"Failed to retrieve page {page}: {response.status_code}")
+            print(f"Failed to retrieve page {current_page}: {response.status_code}")
+            raise Exception(
+                f"Failed to retrieve page {current_page}: {response.status_code}"
+            )
 
         data = response.json()
+        max_page = data["Data"]["MaxPage"]
 
         articles = []
         for article in data["Data"]["CategoryNews"]["Articles"]:
@@ -39,15 +39,19 @@ def fetch_article_content(writer, start_page=1):
                 {
                     "title": article["Title"],
                     "url": article["Link"],
-                    "content": article["BodyStripped"],
+                    "text": article["BodyStripped"],
                 }
             )
 
         writer.writerows(articles)
-        print("Page {}: {} articles".format(page, len(articles)))
+        print("Page {}: {} articles".format(current_page, len(articles)))
 
-        page += 1
-        time.sleep(3)
+        if current_page == max_page:
+            print("Reached the last page.")
+            break
+
+        current_page += 1
+        time.sleep(10)
 
 
 if __name__ == "__main__":
